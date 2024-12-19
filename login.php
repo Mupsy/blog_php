e81f85831624791b7f21a03db61ea645d9bc5bbe
ccyril <cyril.collez@ynov.com>
2024-12-19 14:37:22 +0100

    style login

--- login.php
+++ login.php
@@ -11,6 +11,7 @@
         <meta name="Author" value="Mupsy">
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600&family=Roboto:wght@300&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="./style/index.css">
+        <link rel="stylesheet" href="./style/login.css">
     </head>
     <body>
         <!-- HEADER -->
@@ -28,17 +29,17 @@
             </div>
             
     <!-- Cases -->
-            <div class="info-cards">
-                <div class="info-card">
+            <div class="info-cards-login">
+                <div class="info-card-login">
                     <form action="LoginUser.php" method="POST">
-                        <label for="username">Username</label>
-                        <input type="text" placeholder="Enter Username" name="username" required>
-                        <label for="email">Email</label>
-                        <input type="text" placeholder="Enter ur email" name="email" required>
-                        <label for="password">Password</label>
-                        <input type="password" placeholder="Enter ur Password" name="password" required>
+                        <label for="username">Pseudo :</label>
+                        <input type="text" placeholder="Entrez votre pseudo" name="username" required>
+                        <label for="email">Adresse mail :</label>
+                        <input type="text" placeholder="Entrez votre addresse mail" name="email" required>
+                        <label for="password">Mot de passe :</label>
+                        <input type="password" placeholder="Entre votre mot de passe" name="password" required>
                         <span><a href="./register.php">Si vous n'avez pas de compte, enregistrez-vous ici ! </a></span>
-                        <button type="submit">Login</button>
+                        <button class="login-button" type="submit">Login</button>
                     </form>
                 </div>
             </div>
